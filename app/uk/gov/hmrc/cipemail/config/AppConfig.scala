e6812c75e910e8ff8dd69acb810d95a8b7a99d71
brdgeth <brdgeth@users.noreply.github.com>
2022-04-20 13:57:14 +0100

    CAV-62 - Config for cip url

--- app/uk/gov/hmrc/cipemail/config/AppConfig.scala
+++ app/uk/gov/hmrc/cipemail/config/AppConfig.scala
@@ -18,9 +18,13 @@ package uk.gov.hmrc.cipemail.config
 
 import javax.inject.{Inject, Singleton}
 import play.api.Configuration
+import uk.gov.hmrc.play.bootstrap.config.ServicesConfig
 
 @Singleton
-class AppConfig @Inject()(config: Configuration) {
+class AppConfig @Inject()(config: Configuration, servicesConfig: ServicesConfig) {
 
-  val appName: String = config.get[String]("appName")
+  lazy val appName: String = config.get[String](path = "appName")
+
+  lazy val cipEmailValidationUrl = s"${servicesConfig.baseUrl("cip-email-validation")}"
+  lazy val cipValidateEmail: String = s"$cipEmailValidationUrl${config.get[String]("urls.validate-email")}"
 }
