5e8110dd6e65779db6b590b08761ca31d017819c
brdgeth <brdgeth@users.noreply.github.com>
2022-04-21 15:46:27 +0100

    CAV-62 - work in progress

--- app/uk/gov/hmrc/cipemail/controllers/ValidateEmailProxyController.scala
+++ app/uk/gov/hmrc/cipemail/controllers/ValidateEmailProxyController.scala
@@ -16,69 +16,50 @@
 
 package uk.gov.hmrc.cipemail.controllers
 
+import akka.util.ByteString
 import org.slf4j.LoggerFactory
-import play.api.libs.functional.syntax.unlift
-import play.api.libs.json.{JsObject, JsValue, Json, OWrites, Writes, __}
+import play.api.http.HttpEntity
+import play.api.libs.json.{JsValue, __}
 import play.api.libs.ws.WSResponse
-import play.api.mvc.{AbstractController, Action, ControllerComponents, Result}
+import play.api.mvc.{AbstractController, Action, ControllerComponents, ResponseHeader, Result}
 import uk.gov.hmrc.cipemail.config.AppConfig
 import uk.gov.hmrc.cipemail.service.ValidateEmailProxyService
 
-import java.util.concurrent.CompletionStage
 import javax.inject.{Inject, Singleton}
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.{Future, future}
 
 @Singleton
 class ValidateEmailProxyController @Inject()(cc: ControllerComponents,
-                                             config: AppConfig,
-                                             ws: play.api.libs.ws.WSClient,
                                              validateEmailProxyService: ValidateEmailProxyService)
     extends AbstractController(cc) {
 
   private val logger = LoggerFactory.getLogger(getClass)
 
-/* code in other git repo
- def validateFormat(): Action[JsValue] = Action(parse.json).async { implicit request =>
-    withJsonBody[EmailAddress] { _ => Future.successful(Ok) }
-  }*/
-
-  /*
-  match {
-  case d: Double => d
-  case _  => "NaN"
-}
-   */
-
-  // TODO - ADD IN ERROR HANDLING
-  def validateFormat(): Action[JsValue] = Action(parse.json).async { implicit request =>
-    val futureWrapper = scala.concurrent.Future { validateEmailProxyService.callCipValidateEmailEndpoint(request) }
-    futureWrapper
-   /* futureWrapper match {
-      case response: WSResponse => {
-        val responseStatus = response.status
-       /* val fResult: Future[Result] = futureWrapper.map { r =>
-          r.whenCompleteAsync( w -> w.)
+  implicit def Response2Result(response: Future[WSResponse]): Future[Result] = {
+    response map {
+      response =>
+        val headers = response.allHeaders map {
+          h => (h._1, h._2.head)
         }
-        fResult*/
-      /*  if (responseStatus == 200) {
-          Future.successful(Ok)
-        } else if (responseStatus == 400) {
-          Future.successful(BadRequest(cc.messagesApi("error.invalid")(cc.langs.availables.head)))
-        } else {
-          Future.successful(response)
-        }*/
-
-      }
-      //{Future[Result] = response.json }//Future.successful(response)
-     // case _ => Future.successful(BadRequest(cc.messagesApi("error.invalid")(cc.langs.availables.head)))
-    }*/
-
-
-   /* val fResult: Future[Result] = futureWrapper.map { r =>
-      Json.toJson(r)
+        val result = Result(ResponseHeader(response.status, headers), HttpEntity.Strict(ByteString(response.body), Some("application/json")))
+        result
     }
-    fResult*/
+  }
+
+  def validateFormat(): Action[JsValue] = Action(parse.json).async { implicit request =>
+    val futureWrapper = scala.concurrent.Future {
+      validateEmailProxyService.callCipValidateEmailEndpoint(request)
+    }
+    futureWrapper match {
+      case response: WSResponse => {
+        val futureWrapperForWSResponse = scala.concurrent.Future {
+          response
+        }
+        val result = Response2Result(futureWrapperForWSResponse)
+        result
+      }
+    }
   }
 
 }
